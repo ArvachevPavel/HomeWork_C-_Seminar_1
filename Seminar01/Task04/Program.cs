10dfd9918e2cb6d68ae1a983813d8d9a87aca8f1
Pavel Arvachev <arvachev@gmail.com>
2022-08-19 14:17:28 +0300

    Add Task04

---
+++ Seminar01/Task04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine ("Введите первое любое целое число :");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите второе любое целое число :");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите третье любое целое число :");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+Console.Write("Самое большое число это ");
+Console.Write(max);
